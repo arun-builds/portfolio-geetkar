258a06d21d55a606836b6fe60a3fa29942c738c4
arunprajapati-arch <arunprajapati.mail@gmail.com>
2025-09-23 10:46:08 +0530

    userProfile edit working

--- src/components/ProfileButtons.tsx
+++ src/components/ProfileButtons.tsx
@@ -2,18 +2,32 @@
 import { Pencil } from "lucide-react";
 import ProfileButton from "./ProfileButton";
 import { useEditMode } from "@/contexts/EditModeContext";
+import { DialogTitle, Dialog, DialogTrigger, DialogContent, DialogHeader, DialogDescription } from "./ui/dialog";
 
 export default function ProfileButtons() {
     const { isEditMode, toggleEditMode } = useEditMode();
     return (
         <div className="flex gap-4">
-        <div className={`grid grid-cols-2 max-md:justify-center gap-4 w-full items-end ${isEditMode ? "bg-zinc-800 p-2 rounded-xl border" : ""}`}>
-            <ProfileButton platform="youtube" />
-            <ProfileButton platform="spotify" />
-            <ProfileButton platform="youtube" />
-            <ProfileButton platform="other" />
-        </div>
-        <Pencil size={20} className={`${isEditMode ? "" : "hidden"} `} />
+            <div className={`grid grid-cols-2 max-md:justify-center gap-4 w-full items-end ${isEditMode ? "bg-zinc-800 p-2 rounded-xl border" : ""}`}>
+                <ProfileButton platform="youtube" />
+                <ProfileButton platform="spotify" />
+                <ProfileButton platform="youtube" />
+                <ProfileButton platform="other" />
+            </div>
+            <div>
+                <Dialog>
+                    <DialogTrigger><Pencil size={20} className={`${isEditMode ? "" : "hidden"} hover:cursor-pointer hover:text-yellow-400`} /> </DialogTrigger>
+                    <DialogContent>
+                        <DialogHeader>
+                            <DialogTitle>Edit Links</DialogTitle>
+                            <DialogDescription>
+                                Edit your profile links.
+                            </DialogDescription>
+                        </DialogHeader>
+                    </DialogContent>
+                </Dialog>
+
+            </div>
         </div>
     )
 }
--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -2,26 +2,49 @@
 import UserAvatar from "@/components/UserAvatar";
 import { Pencil } from "lucide-react";
 import { useEditMode } from "@/contexts/EditModeContext";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+import UserEdit from "./UserEdit";
 export default function UserProfile({ userImage, name, username }: { userImage: string, name: string, username: string }) {
     const { isEditMode, toggleEditMode } = useEditMode();
     return (
         <div className="flex gap-4">
-        <div className={`flex items-center  gap-4 ${isEditMode ? "bg-zinc-800 p-2 rounded-xl border" : ""}`}>
+            <div className={`flex items-center  gap-4 ${isEditMode ? "bg-zinc-800 p-2 rounded-xl border" : ""}`}>
 
-            <UserAvatar usedIn="profile" src={userImage} />
+                <UserAvatar usedIn="profile" src={userImage} />
 
-            <div className="flex flex-col gap- ">
-                <div className="flex items-center justify-center gap-2">
-                    <h1 className="text-2xl font-bold">{name}</h1>
-                    <h4 className="text-sm text-gray-500">@{username}</h4>
-                </div>
-                <div>
-                    <p className="text-sm text-gray-100">Music Producer</p>
-                    {/* <p className="text-sm text-gray-100">Mumbai, India</p> */}
+                <div className="flex flex-col gap- ">
+                    <div className="flex items-center justify-center gap-2">
+                        <h1 className="text-2xl font-bold">{name}</h1>
+                        <h4 className="text-sm text-gray-500">@{username}</h4>
+                    </div>
+                    <div>
+                        <p className="text-sm text-gray-100">Music Producer</p>
+                        {/* <p className="text-sm text-gray-100">Mumbai, India</p> */}
+                    </div>
                 </div>
             </div>
-        </div>
-        <Pencil size={20} className={`${isEditMode ? "" : "hidden"}`} />
+            <div>
+            <Dialog >
+                <DialogTrigger><Pencil size={20} className={`${isEditMode ? "" : "hidden"} hover:cursor-pointer hover:text-yellow-400`} /></DialogTrigger>
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogTitle>Edit Profile</DialogTitle>
+                            <DialogDescription>
+                                Edit your profile information.
+                            </DialogDescription>
+                    </DialogHeader>
+                    <UserEdit />
+                    </DialogContent>
+            </Dialog>
+            </div>
+
         </div>
     )
 }
